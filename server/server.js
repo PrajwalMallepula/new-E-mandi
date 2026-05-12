27099ca508dff1c4692d1e4732cfc3c7c361223a
Prajwal_Mallepula <149990416+PrajwalMallepula@users.noreply.github.com>
2026-05-12 20:17:08 +0530

    Fix typo in allowedOrigins for Cloudinary

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://new-e-mandi-final.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://new-e-mandi-finall.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
